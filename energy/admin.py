829681a3b78add51e39f0fb4e15e06bd3c1a3b94
Collin Sinclair <collin@collinsinclair.com>
2024-03-08 16:35:27 -0700

    add user profiles

--- energy/admin.py
+++ energy/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Expenditure, Intake, Weight
+from .models import Expenditure, Intake, UserProfile, Weight
 
 admin.site.register(Intake)
 admin.site.register(Expenditure)
 admin.site.register(Weight)
+admin.site.register(UserProfile)
