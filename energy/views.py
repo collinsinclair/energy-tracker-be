d9c97bab4723359df51d66677db97536606c9b61
Collin Sinclair <collin@collinsinclair.com>
2024-03-08 14:17:49 -0700

    document code

--- energy/views.py
+++ energy/views.py
@@ -15,6 +15,14 @@ from .serializers import ExpenditureSerializer, IntakeSerializer, WeightSerializ
 
 class DateRangeOperationsMixin:
     def get_date_from_request(self, request):
+        """
+        Extracts a date from the given request object's query parameters. If no date is provided,
+        it returns the current date. The function expects the date in the 'YYYY-MM-DD' format.
+
+        @param request: The HttpRequest object containing query parameters.
+        @return: A datetime.date object representing the extracted or current date.
+        @raise ValueError: If the date format is invalid or cannot be parsed.
+        """
         date_str = request.query_params.get("date", None)
         if date_str is None:
             return timezone.now().date()
@@ -25,11 +33,29 @@ class DateRangeOperationsMixin:
             return date
 
     def get_datetime_range_for_date(self, date):
+        """
+        Creates a datetime range for a given date, starting at the beginning of the day (00:00:00)
+        and ending at the end of the day (23:59:59.999999).
+
+        @param date: A datetime.date object for which the datetime range is needed.
+        @return: A tuple containing two datetime.datetime objects representing the start and end
+                 of the given date.
+        """
         datetime_start = datetime.datetime.combine(date, datetime.time.min)
         datetime_end = datetime.datetime.combine(date, datetime.time.max)
         return datetime_start, datetime_end
 
     def aggregate_calories_for_date_range(self, model, datetime_start, datetime_end):
+        """
+        Aggregates the sum of calories for a given model's objects that fall within a specific
+        datetime range.
+
+        @param model: The Django model class to query against.
+        @param datetime_start: A datetime.datetime object representing the start of the range.
+        @param datetime_end: A datetime.datetime object representing the end of the range.
+        @return: The sum of calories for objects within the specified datetime range.
+                 Returns 0 if no objects are found.
+        """
         return (
             model.objects.filter(
                 timestamp__range=(datetime_start, datetime_end)
@@ -38,6 +64,14 @@ class DateRangeOperationsMixin:
         )
 
     def aggregate_daily_calories(self, queryset):
+        """
+        Aggregates daily calories from a queryset, grouping the results by date and summing
+        the calories for each date.
+
+        @param queryset: A Django queryset of objects that have a 'timestamp' and 'calories' field.
+        @return: A QuerySet containing dictionaries with 'date' and 'total_calories' keys,
+                 representing the sum of calories for each day present in the input queryset.
+        """
         return (
             queryset.annotate(date=TruncDay("timestamp"))
             .values("date")
@@ -52,6 +86,16 @@ class IntakeViewSet(viewsets.ModelViewSet, DateRangeOperationsMixin):
 
     @action(detail=False, methods=["get"])
     def daily_sum(self, request):
+        """
+        Provides the sum of calories consumed for a specified date, extracted from the request's
+        query parameters. If no date is provided, it defaults to the current date. This method
+        handles invalid date format errors and returns the total calories for the given date.
+
+        @param request: The HttpRequest object containing query parameters, including an optional
+                        'date' parameter in 'YYYY-MM-DD' format.
+        @return: A Response object containing the 'date' and 'total_calories' for the specified date.
+                 Returns a 400 error response if the date format is invalid.
+        """
         try:
             date = self.get_date_from_request(request)
         except ValueError as e:
@@ -64,6 +108,17 @@ class IntakeViewSet(viewsets.ModelViewSet, DateRangeOperationsMixin):
 
     @action(detail=False, methods=["get"])
     def daily_sums(self, request):
+        """
+        Aggregates and returns the daily sums of calories consumed for each day available in the
+        queryset. This method does not require any date to be provided in the request's query
+        parameters, as it calculates and returns the sum of calories for all dates present in the
+        queryset.
+
+        @param request: The HttpRequest object, used to access any query parameters if necessary.
+                        This method does not use query parameters directly.
+        @return: A Response object containing a list of dictionaries, each with a 'date' and
+                 'total_calories' key, representing the sum of calories consumed for each day.
+        """
         daily_calories = self.aggregate_daily_calories(self.queryset)
         data = [
             {"date": item["date"].isoformat(), "total_calories": item["total_calories"]}
@@ -84,6 +139,18 @@ class WeightViewSet(viewsets.ModelViewSet):
 
 class DailyBalanceView(APIView, DateRangeOperationsMixin):
     def get(self, request):
+        """
+        Calculates and returns the daily balance of calories for a specified date by subtracting the
+        total calorie expenditure from the total calorie intake. The date is extracted from the
+        request's query parameters. If no date is provided, it defaults to the current date. This
+        method also handles invalid date format errors by returning an error response.
+
+        @param request: The HttpRequest object containing query parameters, including an optional
+                        'date' parameter in 'YYYY-MM-DD' format.
+        @return: A Response object containing the 'date', 'total_intake' calories, 'total_expenditure'
+                 calories, and the resulting 'balance' of calories for the specified date. If the
+                 date format is invalid, returns a 400 error response with the error message.
+        """
         try:
             date = self.get_date_from_request(request)
         except ValueError as e:
@@ -107,6 +174,20 @@ class DailyBalanceView(APIView, DateRangeOperationsMixin):
 
 class DailyBalancesView(APIView, DateRangeOperationsMixin):
     def get(self, request):
+        """
+        Retrieves and calculates the daily balances of calories for all available dates by
+        comparing the total calorie intake against the total calorie expenditure for each day.
+        This endpoint aggregates calorie data across all dates present in the Intake and
+        Expenditure models, computes the balance for each day, and returns a list of daily
+        balances.
+
+        @param request: The HttpRequest object. This method does not use any query parameters
+                        from the request, as it operates over all available data.
+        @return: A Response object containing a list of dictionaries. Each dictionary represents
+                 a day's data with the keys 'date', 'total_intake', 'total_expenditure', and
+                 'balance', indicating the day's date, total calorie intake, total calorie
+                 expenditure, and the net balance of calories, respectively.
+        """
         daily_intakes = (
             self.aggregate_daily_calories(Intake.objects.all())
             .annotate(date_only=TruncDay("date"))
