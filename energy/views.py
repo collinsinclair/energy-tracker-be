0a78dc39cce85f1c70a43b819b023f81d62df47f
Collin Sinclair <collin@collinsinclair.com>
2024-03-10 19:25:28 -0600

    add daily intakes endpoint and to summary

--- energy/views.py
+++ energy/views.py
@@ -59,16 +59,6 @@ class IntakeViewSet(viewsets.ModelViewSet, DateRangeOperationsMixin):
 
     @action(detail=False, methods=["get"])
     def daily_sum(self, request):
-        """
-        Provides the sum of calories consumed for a specified date, extracted from the request's
-        query parameters. If no date is provided, it defaults to the current date. This method
-        handles invalid date format errors and returns the total calories for the given date.
-
-        @param request: The HttpRequest object containing query parameters, including an optional
-                        'date' parameter in 'YYYY-MM-DD' format.
-        @return: A Response object containing the 'date' and 'total_calories' for the specified date.
-                 Returns a 400 error response if the date format is invalid.
-        """
         try:
             date = self.get_date_from_request(request)
         except ValueError as e:
@@ -81,23 +71,25 @@ class IntakeViewSet(viewsets.ModelViewSet, DateRangeOperationsMixin):
 
     @action(detail=False, methods=["get"])
     def daily_sums(self, request):
-        """
-        Aggregates and returns the daily sums of calories consumed for each day available in the
-        queryset. This method does not require any date to be provided in the request's query
-        parameters, as it calculates and returns the sum of calories for all dates present in the
-        queryset.
-
-        @param request: The HttpRequest object, used to access any query parameters if necessary.
-                        This method does not use query parameters directly.
-        @return: A Response object containing a list of dictionaries, each with a 'date' and
-                 'total_calories' key, representing the sum of calories consumed for each day.
-        """
         daily_calories = self.aggregate_daily_calories(self.queryset)
         data = [
             {"date": item["date"].isoformat(), "total_calories": item["total_calories"]}
             for item in daily_calories
         ]
         return Response(data)
+
+    @action(detail=False, methods=["get"])
+    def todays_intakes(self, request):
+        try:
+            date = self.get_date_from_request(request)
+        except ValueError as e:
+            return Response({"error": str(e)}, status=400)
+        datetime_start, datetime_end = self.get_datetime_range_for_date(date)
+        todays_intakes = self.queryset.filter(
+            timestamp__range=(datetime_start, datetime_end)
+        )
+        serializer = self.get_serializer(todays_intakes, many=True)
+        return Response(serializer.data)
 
 
 class ExpenditureViewSet(viewsets.ModelViewSet):
@@ -112,18 +104,6 @@ class WeightViewSet(viewsets.ModelViewSet):
 
 class DailyBalanceView(APIView, DateRangeOperationsMixin):
     def get(self, request):
-        """
-        Calculates and returns the daily balance of calories for a specified date by subtracting the
-        total calorie expenditure from the total calorie intake. The date is extracted from the
-        request's query parameters. If no date is provided, it defaults to the current date. This
-        method also handles invalid date format errors by returning an error response.
-
-        @param request: The HttpRequest object containing query parameters, including an optional
-                        'date' parameter in 'YYYY-MM-DD' format.
-        @return: A Response object containing the 'date', 'total_intake' calories, 'total_expenditure'
-                 calories, and the resulting 'balance' of calories for the specified date. If the
-                 date format is invalid, returns a 400 error response with the error message.
-        """
         try:
             date = self.get_date_from_request(request)
         except ValueError as e:
@@ -147,20 +127,6 @@ class DailyBalanceView(APIView, DateRangeOperationsMixin):
 
 class DailyBalancesView(APIView, DateRangeOperationsMixin):
     def get(self, request):
-        """
-        Retrieves and calculates the daily balances of calories for all available dates by
-        comparing the total calorie intake against the total calorie expenditure for each day.
-        This endpoint aggregates calorie data across all dates present in the Intake and
-        Expenditure models, computes the balance for each day, and returns a list of daily
-        balances.
-
-        @param request: The HttpRequest object. This method does not use any query parameters
-                        from the request, as it operates over all available data.
-        @return: A Response object containing a list of dictionaries. Each dictionary represents
-                 a day's data with the keys 'date', 'total_intake', 'total_expenditure', and
-                 'balance', indicating the day's date, total calorie intake, total calorie
-                 expenditure, and the net balance of calories, respectively.
-        """
         daily_intakes = (
             self.aggregate_daily_calories(Intake.objects.all())
             .annotate(date_only=TruncDay("date"))
@@ -221,53 +187,45 @@ class RemainingDailyCalories(APIView, DateRangeOperationsMixin):
         )
 
 
+date_range_ops = DateRangeOperationsMixin()
+
+
 @api_view(["GET"])
 def daily_summary(request):
-    """
-    Endpoint for retrieving a daily summary including total calories consumed,
-    total calorie expenditure, calorie goal, and remaining calories until goal
-    for a specified or current date.
-
-    @param request: The HttpRequest object containing an optional 'date' parameter.
-    @return: A Response object with the daily summary.
-    """
     try:
-        date = request.query_params.get("date", None)
-        if date:
-            date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
-        else:
-            date = datetime.datetime.now().date()
+        date = date_range_ops.get_date_from_request(request)
     except ValueError as e:
-        return Response({"error": "Invalid date format. Use 'YYYY-MM-DD'."}, status=400)
-
-    datetime_start, datetime_end = (
-        DateRangeOperationsMixin().get_datetime_range_for_date(date)
-    )
-
-    total_intake = DateRangeOperationsMixin().aggregate_calories_for_date_range(
+        return Response({"error": str(e)}, status=400)
+    datetime_start, datetime_end = date_range_ops.get_datetime_range_for_date(date)
+    total_intake = date_range_ops.aggregate_calories_for_date_range(
         Intake, datetime_start, datetime_end
     )
-
     expenditure_record = Expenditure.objects.filter(date=date).aggregate(
         total_expenditure=Sum("calories")
     )
-    total_expenditure = (
-        expenditure_record["total_expenditure"]
-        if expenditure_record["total_expenditure"]
-        else 0
-    )
-
+    total_expenditure = expenditure_record.get("total_expenditure", 0) or 0
     user_profile = UserProfile.objects.first()
     goal_daily_calorie_delta = user_profile.goal_daily_calorie_delta
     calorie_goal = total_expenditure + goal_daily_calorie_delta
     remaining_calories = calorie_goal - total_intake
-
+    todays_intakes = Intake.objects.filter(
+        timestamp__range=(datetime_start, datetime_end)
+    )
+    todays_intakes_data = [
+        {
+            "id": intake.id,
+            "label": intake.label,
+            "calories": intake.calories,
+            "timestamp": intake.timestamp,
+        }
+        for intake in todays_intakes
+    ]
     summary_data = {
         "date": date.isoformat(),
         "total_calories_consumed": total_intake,
         "total_expenditure": total_expenditure,
         "calorie_goal": calorie_goal,
         "remaining_calories_until_goal": remaining_calories,
+        "todays_intakes": todays_intakes_data,  # Include today's intakes data
     }
-
     return Response(summary_data)
